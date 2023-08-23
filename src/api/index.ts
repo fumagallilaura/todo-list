6d54aed2694ce93175bf9ed5b733e97c55d14d0d
Laura Fumagalli <la_12_msn@hotmail.com>
2023-08-22 23:16:26 -0300

    ✨ feat: add security, compression and urlencoded config in index.ts

--- src/api/index.ts
+++ src/api/index.ts
@@ -1,15 +1,21 @@
+import compression from 'compression';
 import express from 'express';
+import 'express-async-errors';
+import helmet from 'helmet';
+import { cors, errorHandler } from './middlewares';
 import routes from './routes';
-import { cors } from './middlewares';
-
 
 const api = express();
 
 // input middlewares
 api.use(express.json());
+api.use(express.urlencoded({ extended: true }));
+api.use(compression({ threshold: 0 }));
+api.use(helmet());
 api.use(cors);
-
 // root router
 api.use('/', routes);
+// output middlewares
+api.use(errorHandler);
 
 export default api;
