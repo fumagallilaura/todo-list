221833d5b64ac796b2ba79a5dfebec9ae1329cc2
Laura Fumagalli <la_12_msn@hotmail.com>
2023-08-22 22:11:43 -0300

    🔧 chore: add environments file and lint aditional configurations

---
+++ src/vars.ts
@@ -0,0 +1,19 @@
+import packageJson from 'package.json';
+const { env, cwd } = process
+
+export default {
+  api: {
+    name: packageJson.name,
+    path: cwd(),
+    port: Number(env.API_PORT || env.PORT || 3001)
+  },
+  db: {
+    username: env.DB_USER || 'root',
+    password: env.DB_PASS || 'root',
+    database: env.DB_NAME || 'todos',
+    host: env.DB_HOST || 'localhost',
+    port: Number(env.DB_PORT || 3306),
+    dialect: env.DB_DIALECT || 'mysql',
+    dialectOptions: env.DB_OPTIONS ? JSON.parse(env.DB_OPTIONS) : {},
+  },
+}
